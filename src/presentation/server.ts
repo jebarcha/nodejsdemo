157e93ca508c0614d63a33c864ebbef29c58d8a4
jebarcha <jebarcha@hotmail.com>
2024-06-11 13:16:30 -0600

    implemented multilogs

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,6 @@
-import { envs } from '../config/plugins/envs.plugin';
 import { LogSeverityLevel } from '../domain/entities/log.entity';
 import { CheckService } from '../domain/use-cases/checks/check-service';
+import { CheckServiceMultiple } from '../domain/use-cases/checks/check-service-multiple';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { MongoLogDatasource } from '../infrastructure/datasources/mongo-log.datasource';
@@ -9,11 +9,12 @@ import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository
 import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email.service';
 
-const logRepository = new LogRepositoryImpl(
-  //new FileSystemDatasource()
-  // new MongoLogDatasource()
+const fslogRepository = new LogRepositoryImpl(new FileSystemDatasource());
+const mongologRepository = new LogRepositoryImpl(new MongoLogDatasource());
+const postgreslogRepository = new LogRepositoryImpl(
   new PostgresLogDatasource()
 );
+
 const emailService = new EmailService();
 
 export class Server {
@@ -37,11 +38,21 @@ export class Server {
     // const logs = await logRepository.getLogs(LogSeverityLevel.low);
     // console.log(logs);
 
+    // CronService.createJob('*/5 * * * * *', () => {
+    //   const url = 'https://google.com';
+
+    //   new CheckService(
+    //     logRepository,
+    //     () => console.log(`${url} is ok`),
+    //     (error) => console.log(error)
+    //   ).execute(url);
+    // });
+
     CronService.createJob('*/5 * * * * *', () => {
       const url = 'https://google.com';
 
-      new CheckService(
-        logRepository,
+      new CheckServiceMultiple(
+        [fslogRepository, mongologRepository, postgreslogRepository],
         () => console.log(`${url} is ok`),
         (error) => console.log(error)
       ).execute(url);
